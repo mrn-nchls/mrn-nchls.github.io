9db20c9b8445cae95af9349ab6f5ecf901e1d3b7
Mrn Nchls <tx.mnichols@gmail.com>
2025-12-02 14:28:06 -0500

    update all

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-// Load saved theme
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark");
-}
+document.addEventListener("DOMContentLoaded", () => {
 
-// change icons
-function updateIcons() {
-    const isDark = document.body.classList.contains("dark");
-
-    document.getElementById("linkedin-icon").src =
-        isDark ? "linkedinlogo_alt.png" : "linkedinlogo.png";
-
-    document.getElementById("gmail-icon").src =
-        isDark ? "gmaillogo_alt.png" : "gmaillogo.png";
-
-    document.getElementById("cv-icon").src =
-        isDark ? "cvlogo_alt.png" : "cvlogo.png";
-}
-
-updateIcons();
-
-// Toggle theme
-document.getElementById("theme-toggle").addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-
-    // Save preference
-    if (document.body.classList.contains("dark")) {
-        localStorage.setItem("theme", "dark");
-    } else {
-        localStorage.setItem("theme", "light");
+    // Load saved theme
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark");
     }
-    
+
+    function updateIcons() {
+        const isDark = document.body.classList.contains("dark");
+
+        document.getElementById("linkedin-icon").src =
+            isDark ? "linkedinlogo_alt.png" : "linkedinlogo.png";
+
+        document.getElementById("gmail-icon").src =
+            isDark ? "gmaillogo_alt.png" : "gmaillogo.png";
+
+        document.getElementById("cv-icon").src =
+            isDark ? "cvlogo_alt.png" : "cvlogo.png";
+    }
+
+    // set icons correctly on page load
     updateIcons();
+
+    // Toggle theme
+    document.getElementById("theme-toggle").addEventListener("click", () => {
+        document.body.classList.toggle("dark");
+
+        // Save preference
+        localStorage.setItem(
+            "theme",
+            document.body.classList.contains("dark") ? "dark" : "light"
+        );
+
+        updateIcons();
+    });
 });
