3f5185f17e21f14bf2b72ca999355f367bea36d0
Mrn Nchls <tx.mnichols@gmail.com>
2025-12-02 14:20:23 -0500

    update all

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark");
 }
 
+// change icons
+function updateIcons() {
+    const isDark = document.body.classList.contains("dark");
+
+    document.getElementById("linkedin-icon").src =
+        isDark ? "linkedinlogo_alt.png" : "linkedinlogo.png";
+
+    document.getElementById("gmail-icon").src =
+        isDark ? "gmaillogo_alt.png" : "gmaillogo.png";
+
+    document.getElementById("cv-icon").src =
+        isDark ? "cvlogo_alt.png" : "cvlogo.png";
+}
+
+updateIcons();
+
 // Toggle theme
 document.getElementById("theme-toggle").addEventListener("click", () => {
     document.body.classList.toggle("dark");
@@ -13,4 +29,6 @@ document.getElementById("theme-toggle").addEventListener("click", () => {
     } else {
         localStorage.setItem("theme", "light");
     }
+    
+    updateIcons();
 });
